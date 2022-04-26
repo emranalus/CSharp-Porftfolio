97d07f6b9b7b3446f0340a621d6cafebeb6f6db8
emirhan.alus <emirhan.alus@5144-L102-PC02>
2022-04-26 13:53:57 +0300

    First App

--- WinFormsAppPractice/Form1.cs
+++ WinFormsAppPractice/Form1.cs
@@ -17,5 +17,29 @@ namespace WinFormsAppPractice
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> araclar = new List<string>();
+            araclar.Add("80 Model Corvette");
+            araclar.Add("Kia Sportage");
+            araclar.Add("Rolls Royce Phantom");
+            araclar.Add("Honda Civic");
+
+            foreach (var araba in araclar)
+            {
+                listBox1.Items.Add(araba);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+
+        }
     }
 }
