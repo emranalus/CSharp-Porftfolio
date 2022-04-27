5aad778071af7c87306ba2b1582ec6d0b66f9ec2
emirhan.alus <emirhan.alus@5144-L102-PC02>
2022-04-27 12:24:09 +0300

    Added Summary

--- MethodsLecture/Program.cs
+++ MethodsLecture/Program.cs
@@ -6,7 +6,31 @@ namespace MethodsLecture
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int toplam = Sum(3, 5);
+            Console.WriteLine(toplam);
+            Console.WriteLine(Sum("a","b"));
+
+        }
+
+        public static int Sum(int sayi1, int Sayi2)
+        {
+            return sayi1 + Sayi2;
+        }
+
+        public static int Sum(float sayi1, int sayi2)
+        {
+            return ((int)sayi1 + sayi2);
+        }
+
+        /// <summary>
+        /// Sums up two string value
+        /// </summary>
+        /// <param name="a">String Tipinden İlk Değer</param>
+        /// <param name="b">Second String Value</param>
+        /// <returns>İki Stringi Toplar</returns>
+        public static string Sum(string a, string b)
+        {
+            return a + b;
         }
     }
 }
