b0b1102c88ffeeaafd7040e1e435956e476b64b8
emirhan.alus <emirhan.alus@5144-L102-PC02>
2022-04-27 13:55:11 +0300

    asal sayilar

--- MethodsLecture/Program.cs
+++ MethodsLecture/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MethodsLecture
 {
@@ -6,9 +7,13 @@ namespace MethodsLecture
     {
         static void Main(string[] args)
         {
-            int toplam = Sum(3, 5);
-            Console.WriteLine(toplam);
-            Console.WriteLine(Sum("a","b"));
+            //int toplam = Sum(3, 5);
+            //Console.WriteLine(toplam);
+            //Console.WriteLine(Sum("a","b"));
+
+            AsalBul();
+
+            
 
         }
 
@@ -32,5 +37,61 @@ namespace MethodsLecture
         {
             return a + b;
         }
+
+        //Sadece 1'e ve kendine bölünebilen pozitif tam sayılara asal sayılar denir.
+        //Kod ile bir sayının 1'e ve kendine bölünemediği nasıl anlaşılır
+        //önce kontrol sonra basım yapılabilir
+
+        //rastgele[i] nin içindeki her sayıyı 1 den başlayarak listenin uzunluğuna kadar olan ardaşık sayılar ile böl
+        //her sayı 1'e ve kendisine bölünür
+        //1 ve kendisi hariç bir sayı ile bölümü 0 eden bir sayı asal olamaz
+        //her sayı 1'e bölünür
+        //bir sayı ile bölümü sonucu 0 sa ve o sayı kendisi değilse o sayı asal değildir
+
+        //4 sayısı 2 sayısı ile bölünürse asal değildir
+        public static void AsalBul()
+        {
+            int[] rastgele = new int[50];
+            List<int> asallar = new List<int>();
+            
+            bool asalDegil;
+            int sayi = 3;
+            int sayac = 1;
+            Random rand = new Random();
+
+            // Listenin içinin doldurulması
+            for (int i = 1; i < rastgele.Length; i++)
+            {
+                rastgele[i] = rand.Next(1, 51);
+            }
+
+            // Listedeki değişkenlerin kontrolu
+            // 1 ve kendisi hariç bir sayıya bölüneni bul o sayı asal değil elsi bas
+            for (int i = 1; i < rastgele.Length; i++)
+            {
+
+                for (int j = 2; j < rastgele.Length; j++)
+                {
+                    // sayinin j ile bolumu 0 ise ve sayi j den farklı ise sayı asaldır
+                    if (rastgele[i] % j == 0 && j != rastgele[i])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        asallar.Add(rastgele[i]);
+                        break;
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < asallar.Count; i++)
+            {
+                Console.WriteLine(asallar[i]);
+            }
+
+        }
+
     }
 }
