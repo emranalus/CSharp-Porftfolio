2f90d32b30d7d4ce2544de320b10b1e167e5507e
emirhan.alus <emirhan.alus@5144-L102-PC02>
2022-04-28 12:19:26 +0300

    Add WinSansur Project

---
+++ WinSansur/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSansur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Sansür Kelimesi Ekle - Sevgi Bakanlığı
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox2.Text);
+        }
+
+        // Sansürle - George Orwell Style
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string item in listBox1.Items)
+            {
+                textBox1.Text = textBox1.Text.Replace(item, "***");
+            }
+        }
+    }
+}
