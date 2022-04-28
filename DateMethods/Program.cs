e70111910bc99511c540592fe9f471ac0ef666fd
emirhan.alus <emirhan.alus@5144-L102-PC02>
2022-04-28 13:00:02 +0300

    Math Library

---
+++ DateMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace DateMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Math.Floor aşağıya yuvarla 
+            // Math.Ceiling yukarıya yuvarla
+            // Math.Round sayıyı en yakına yuvarlar
+            // Sayinini kusurati .50 ise tek mi cift mi olduguna bakilir
+            // eger tek ise yukarı cift ise asagı yuvarlama yapilir
+        }
+    }
+}
