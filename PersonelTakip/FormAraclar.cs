0a2269607aec80d53d82d4ee30e8b87f2ffac3ca
emirhan.alus <emirhan.alus@5144-L102-PC02>
2022-05-11 12:21:17 +0300

    Funniest Shit I Ever Seen

--- PersonelTakip/FormAraclar.cs
+++ PersonelTakip/FormAraclar.cs
@@ -16,6 +16,16 @@ namespace PersonelTakip
         string marka, model, renk, kasa, vites
             , guvenlikPaketi, donanimPaketi, motor, yakitTipi;
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
         // Kasa Tipleri: Cupe,Sedan,Cabrio,Hatchbag,Station Wagon
         public enum KasaTipi
         {
@@ -24,13 +34,24 @@ namespace PersonelTakip
             Hatchbag, 
             Station
         }
-
         public enum YakitTipi
         {
             Benzin,
             Motorin,
             Elektrik,
             Hybrit
+        }
+        public enum DonanimPaketi
+        {
+            Full,
+            FullArtiFull,
+            Bos
+        }
+        public enum VitesTipi
+        {
+            Ototmatik,
+            Manuel,
+            YariOtomatik
         }
 
         struct Araba
@@ -39,17 +60,20 @@ namespace PersonelTakip
             public string Model;
             public string Renk;
             public KasaTipi Kasa;
-            public string Vites;
+            public VitesTipi Vites;
             public string GuvenlikPaketi;
-            public string DonanimPaketi;
+            public DonanimPaketi Paket;
             public string Motor;
-            public YakitTipi YakitTipi;
+            public YakitTipi Yakit;
         }
         private void button1_Click(object sender, EventArgs e)
         {
 
             Araba arac1 = new Araba();
-            arac1.Kasa = KasaTipi.Sedan;
+            arac1.Kasa  = KasaTipi.Sedan;
+            arac1.Yakit = YakitTipi.Elektrik;
+            arac1.Paket = DonanimPaketi.Full;
+            arac1.Vites = VitesTipi.Ototmatik;
 
         }
 
@@ -60,7 +84,16 @@ namespace PersonelTakip
 
         private void FormAraclar_Load(object sender, EventArgs e)
         {
+            cmbDonanim.Items.AddRange(Enum.GetNames(typeof(DonanimPaketi)));
+            cmbDonanim.SelectedIndex = 0;
 
+            cmbKasa.Items.AddRange(Enum.GetNames(typeof(KasaTipi)));
+            cmbKasa.SelectedIndex = 0;
+
+            cmbVites.Items.AddRange(Enum.GetNames(typeof (VitesTipi)));
+            cmbVites.SelectedIndex = 0;
+
+            
         }
     }
 }
