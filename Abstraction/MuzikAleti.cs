27ca6d669782db631284fb4a2ef8bd455e3fe152
emirhan.alus <emirhan.alus@5144-L102-PC02>
2022-05-16 12:32:53 +0300

    Abstration example

---
+++ Abstraction/MuzikAleti.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    // Abstact clasess are base classes so only thing an abstact class can do is inheritence
+    // And no instancing,privatization is allowed
+    internal abstract class MuzikAleti
+    {
+
+        public string Marka { get; set; }
+        public string Aciklama { get; set; }
+
+        // Abstract forces the function to be defined inside of the child class for child class
+        // via override
+        public abstract string Cal();
+
+    }
+}
