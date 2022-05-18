ae4f98c773511473c8068f90543f56485b93a826
emirhan.alus <emirhan.alus@5144-L102-PC02>
2022-05-18 12:00:09 +0300

    OCP

---
+++ OCP/GoodCoffee/Abstract/GoodCoffeeShop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP.GoodCoffee.Abstract
+{
+    public abstract class GoodCoffeeShop
+    {
+
+        public abstract double GetTotalPrice(int adet);
+
+    }
+}
---
+++ SRP/GoodExample/GoodEmployee.cs
@@ -0,0 +1,41 @@
+﻿using SRP.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP.GoodExample
+{
+    internal class GoodEmployee
+    {
+
+        Logger logger;
+        string log;
+        
+        public GoodEmployee()
+        {
+            logger = new Logger();
+        }
+
+        public void CreateEmployee(Employee calisan)
+        {
+            try
+            {
+                string yazilacakSatir = calisan.Id + ";" + calisan.Name + ";"
+                    + calisan.Surname + ";" + calisan.Salary;
+
+                logger.LogFile("Calisanlar.txt", yazilacakSatir);
+                log = "Kayit Tarihi" + DateTime.Now + ";" + calisan.Id;
+                logger.LogFile("Log.txt", log);
+            }
+            catch (Exception ex)
+            {
+                log = "Kayit Tarihi" + DateTime.Now + ";" + calisan.Id;
+                logger.LogFile("Hata.txt", log);
+            }
+        }
+
+    }
+}
